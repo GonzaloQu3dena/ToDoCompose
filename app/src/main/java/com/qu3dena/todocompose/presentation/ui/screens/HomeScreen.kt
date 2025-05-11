123458151479024d4651e9716e7ed8c5c11c9bf5
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-05-11 12:03:24 -0500

    refactor: remove fab from home screen.

--- app/src/main/java/com/qu3dena/todocompose/presentation/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/qu3dena/todocompose/presentation/ui/screens/HomeScreen.kt
@@ -2,39 +2,19 @@ package com.qu3dena.todocompose.presentation.ui.screens
 
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.fillMaxSize
 
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.Alignment
-
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-
-import com.qu3dena.todocompose.shared.components.GenericFloatingActionButton
 
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
-    onNavigationToForm: () -> Unit,
 ) {
     Box(
         modifier = modifier
             .fillMaxSize()
     ) {
         Text(text = "Home Screen")
-        GenericFloatingActionButton(
-            modifier = Modifier.align(Alignment.BottomEnd).padding(16.dp),
-            onClick = onNavigationToForm,
-            icon = {
-                Icon(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = null
-                )
-            }
-        )
     }
 }
